3f39b9178e590846266db23dd43574347b4397e7
Alexei <hlbu@yandex.ru>
2022-07-16 21:26:25 +0300

    assedt added

--- pages/text_box_page.py
+++ pages/text_box_page.py
@@ -1,5 +1,6 @@
 import time
 
+from generator.generator import generated_person
 from locators.locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
@@ -8,12 +9,18 @@ class TextBoxPage(BasePage):
     locator = TextBoxPageLocators()
 
     def fill_all_fields(self):
-        self.element_is_visible(self.locator.FULL_NAME).send_keys("sqwe")
-        self.element_is_visible(self.locator.EMAIL).send_keys("sadf@sdgfs.ru")
-        self.element_is_visible(self.locator.CURRENT_ADDRESS).send_keys("asdfasdfa")
-        self.element_is_visible(self.locator.Permanent_Address).send_keys("fsdfgfdgfg")
+        person_info = next(generated_person())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+        self.element_is_visible(self.locator.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locator.EMAIL).send_keys(email)
+        self.element_is_visible(self.locator.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locator.Permanent_Address).send_keys(permanent_address)
         self.element_is_visible(self.locator.SUBMIT_BUTTON).click()
         time.sleep(2)
+        return full_name, email, current_address, permanent_address
 
     def check_filled_forms(self):
         CREATED_FULL_name = self.element_is_present(self.locator.CREATED_FULL_NAME).text.split(":")[1]
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -4,10 +4,10 @@ from pages.text_box_page import TextBoxPage
 def test1(driver):
     text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
     text_box_page.open()
-    text_box_page.fill_all_fields()
+    full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
     output_FULL_name, output_email, output_CURRENT_address, output_Permanent_address = \
         text_box_page.check_filled_forms()
-    print(output_Permanent_address)
-    print(output_email)
-    print(output_CURRENT_address)
-    print(output_Permanent_address)
+    assert full_name == output_FULL_name, "данные full_name не равны"
+    assert email == output_email, "данные email не равны"
+    assert current_address == output_CURRENT_address, "данные current_address не равны"
+    assert permanent_address == output_Permanent_address, "данные permanent_address не равны"
